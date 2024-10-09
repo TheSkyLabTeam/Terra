dadca6dede77a8e16042982a3538955a2668b6df
daniherreraa <herreraacevedodl@gmail.com>
2024-10-09 08:21:42 -0500

    New endpoints. Railway API

--- app/(app)/global/page.jsx
+++ app/(app)/global/page.jsx
@@ -36,7 +36,7 @@ export default function Page() {
       try {
         setLoading(true);
         const response = await fetch(
-          `https://api-hackathon-fuego.onrender.com/obtener_datos_firms/?country=${selectedCountry}&start_date=${dateRange.startDate}&end_date=${dateRange.endDate}`
+          `https://apihackathonfuego-production.up.railway.app/obtener_datos_firms/?country=${selectedCountry}&start_date=${dateRange.startDate}&end_date=${dateRange.endDate}`
         );
 
         if (!response.ok) {
